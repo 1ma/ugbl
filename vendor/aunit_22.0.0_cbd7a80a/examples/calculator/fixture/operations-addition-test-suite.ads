4debbbf11fb34fa40e168cb955b7af3405af998e
Marcel Hernandez <e6990620@gmail.com>
2022-03-25 14:25:23 +0100

    replace aunit hard copy with a submodule

--- vendor/aunit_22.0.0_cbd7a80a/examples/calculator/fixture/operations-addition-test-suite.ads
+++
@@ -1,7 +0,0 @@
---
---  Copyright (C) 2008, AdaCore
---
-with Operations.Binary.Gen_Test.Gen_Suite;
-with Operations.Addition.Test;
-package Operations.Addition.Test.Suite is new
-  Operations.Addition.Test.Gen_Suite ("Operations.Addition");
--- vendor/aunit_22.0.0_cbd7a80a/examples/calculator/tests/operations-addition-test.ads
+++
@@ -1,8 +0,0 @@
---
---  Copyright (C) 2008, AdaCore
---
-with Operations.Binary.Gen_Test;
-with Operations.Addition;
-with Operations.Addition_Test_Fixture; use Operations.Addition_Test_Fixture;
-package Operations.Addition.Test is new Operations.Addition.Gen_Test
-  (Set_Up);
