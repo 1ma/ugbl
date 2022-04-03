1b977e0fe4d372ea687f879c490b58d95f31bd95
Marcel Hernandez <e6990620@gmail.com>
2022-04-03 19:15:50 +0200

    tighten the bolts on Free_Point

--- src/elliptic_curves.adb
+++ src/elliptic_curves.adb
@@ -8,11 +8,14 @@ package body Elliptic_Curves is
       return abs (L - R) < Cmp_Epsilon;
    end "=";
 
+   function At_Infinity (P : Free_Point) return Boolean is
+   begin
+      return P.X = null and P.Y = null;
+   end At_Infinity;
+
    function On_Curve (P : Free_Point) return Boolean is
    begin
-      return
-         (P.X = null and P.Y = null) or else
-         ((P.X /= null and P.Y /= null) and P.Y.all * P.Y.all = P.X.all * P.X.all * P.X.all + P.A * P.X.all + P.B);
+      return At_Infinity (P) or else P.Y.all * P.Y.all = P.X.all * P.X.all * P.X.all + P.A * P.X.all + P.B;
    end On_Curve;
 
    function On_Same_Curve (L, R : On_Curve_Point) return Boolean is
--- src/elliptic_curves.ads
+++ src/elliptic_curves.ads
@@ -7,11 +7,22 @@ package Elliptic_Curves is
    overriding function "=" (L, R : Checked_Float) return Boolean;
 
    type Float_Access is access Checked_Float;
+   --  A null pointer will represent the infinity value.
+   --  There probably is a less obtuse way to go about this.
 
    type Free_Point is record
       X, Y : Float_Access;
       A, B : Checked_Float;
-   end record;
+   end record
+      with Dynamic_Predicate =>
+         (X /= null or Y = null) and
+         (Y /= null or X = null) and
+         (X = null or Y /= null) and
+         (Y = null or X /= null);
+   --  The Dynamic_Predicate enforces that "X is null <==> Y is null" and "X is float <==> Y is float"
+   --  This guarantees that for each Free_Point either X and Y will be null or none will.
+
+   function At_Infinity (P : Free_Point) return Boolean;
 
    function On_Curve (P : Free_Point) return Boolean;
 
@@ -19,7 +30,7 @@ package Elliptic_Curves is
       with Dynamic_Predicate => On_Curve (On_Curve_Point);
 
    subtype Point_At_Infinity is On_Curve_Point
-      with Dynamic_Predicate => Point_At_Infinity.X = null and Point_At_Infinity.Y = null;
+      with Dynamic_Predicate => At_Infinity (Point_At_Infinity);
 
    function On_Same_Curve (L, R : On_Curve_Point) return Boolean;
 
